555a8d377f38da7001867ab70be2b070e5ea2275
Shingo SUZUKI <r2.ichimal@gmail.com>
2014-03-03 08:30:33 +0900

    Limit applicable I/O method depends on setting

--- src/stream.lisp
+++ src/stream.lisp
@@ -1,13 +1,51 @@
 (cl:in-package #:trivial-dump-streams)
 
-(defclass dump-stream (fundamental-binary-input-stream
-                       fundamental-binary-output-stream
-                       fundamental-character-input-stream
-                       fundamental-character-output-stream )
+(defclass dump-stream ()
   ((context :initarg :context :initform (error "required :context"))
    (outbound-buffer :initform (make-queue :simple-queue))
    (temporary-buffer :initform 0)
    (temporary-buffer-length :initform 0) ))
+
+(defclass from-character-dump-stream (fundamental-character-output-stream)
+  () )
+
+(defclass from-binary-dump-stream (fundamental-binary-output-stream)
+  () )
+
+(defclass to-character-dump-stream (fundamental-character-input-stream)
+  () )
+
+(defclass to-binary-dump-stream (fundamental-binary-input-stream)
+  () )
+
+(defclass character-to-binary-dump-stream
+  (dump-stream from-character-dump-stream to-character-dump-stream)
+  () )
+
+(defclass binary-to-character-dump-stream
+  (dump-stream from-binary-dump-stream to-character-dump-stream)
+  () )
+
+(defclass character-to-character-dump-stream
+  (dump-stream from-character-dump-stream to-character-dump-stream)
+  () )
+
+(defclass binary-to-binary-dump-stream
+  (dump-stream from-binary-dump-stream to-binary-dump-stream)
+  () )
+
+(defmethod initialize-instance :after ((obj dump-stream)
+                                       &key &allow-other-keys )
+  (let ((target (with-slots (context) obj
+                  (with-slots (from to) context
+                    (if (eq from :binary)
+                      (if (eq to :binary)
+                        'binary-to-binary-dump-stream
+                        'binary-to-character-dump-stream )
+                      (if (eq to :binary)
+                        'character-to-binary-dump-stream
+                        'character-to-character-dump-stream ))))))
+    (change-class obj target)))
 
 (defun make-dump-stream (from to &key inbound-bits outbound-bits )
   (make-instance 'dump-stream
@@ -18,21 +56,35 @@
                :outbound-bits outbound-bits )))
 
 #+clisp
-(defmethod gray:stream-read-byte-lookahead ((stream dump-stream))
+(defmethod gray:stream-read-byte-lookahead ((stream to-binary-dump-stream))
   (with-slots (outbound-buffer) stream
     (plusp (qsize outbound-buffer)) ))
 
 #+clisp
-(defmethod gray:stream-read-byte-no-hang ((stream dump-stream))
+(defmethod gray:stream-read-byte-no-hang ((stream to-binary-dump-stream))
   (with-slots (outbound-buffer) stream
     (qpop outbound-buffer) ))
 
-(defmethod stream-read-byte ((stream dump-stream))
+(defmethod stream-read-byte ((stream to-binary-dump-stream))
   (loop until (stream-read-byte-lookahead stream)
         do (sleep 0.001) )
   (stream-read-byte-no-hang stream) )
 
-(defmethod stream-write-char ((stream dump-stream) (c character))
+(defmethod stream-read-char-will-hang-p ((stream to-character-dump-stream))
+  (with-slots (outbound-buffer) stream
+    (plusp (qsize outbound-buffer)) ))
+
+(defmethod stream-read-char-no-hang ((stream to-character-dump-stream))
+  (with-slots (outbound-buffer) stream
+    ;; stub
+    (format nil "~x" (qpop outbound-buffer)) ))
+
+(defmethod stream-read-char ((stream to-character-dump-stream))
+  (loop until (stream-read-char-will-hang-p stream)
+        do (sleep 0.001) )
+  (stream-read-char-no-hang stream) )
+
+(defmethod stream-write-char ((stream from-character-dump-stream) (c character))
   (unless (dump-stream-ignoring-char-p stream c)
     (dump-stream-write-char stream c)))
 
